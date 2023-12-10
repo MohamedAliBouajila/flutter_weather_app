18ff8ddac132014be377d3a88da2f7215fbe4547
Mohamed Ali Bouajila <bouajilamedali00@gmail.com>
2023-12-11 00:26:21 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/screens/home_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async{
+   await dotenv.load();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(   
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
